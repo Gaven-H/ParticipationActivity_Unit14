f42e1724f3f4944c6f38041abdf78e82c792baa8
Gaven <ghuynh@student.cscc.edu>
2026-04-26 17:27:13 -0400

    Set up button to start game and make mouse disappear, also ad pycache to .gitignore, made sure no keydown events are captured till button press

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,7 @@ from arsenal import Arsenal
 from alien_fleet import AlienFleet
 from game_stats import GameStats
 from time import sleep
+from button import Button
 
 class AlienInvasion:
     def __init__(self) -> None:
@@ -37,7 +38,9 @@ class AlienInvasion:
         self.ship = Ship(self, Arsenal(self))
         self.alien_fleet = AlienFleet(self)
         self.alien_fleet.create_fleet()
-        self.game_active = True
+
+        self.play_button = Button(self, 'Play')
+        self.game_active = False
 
     def run_game(self) -> None:
         # Game Loop
@@ -68,7 +71,7 @@ class AlienInvasion:
         if self.alien_fleet.check_destroyed_status():
             self._reset_level()
         
-    def _check_game_status(self):
+    def _check_game_status(self) -> None:
         if self.game_stats.ships_left > 0:
             self.game_stats.ships_left -= 1
             self._reset_level()
@@ -77,6 +80,15 @@ class AlienInvasion:
             self.game_active = False
 
         print(self.game_stats.ships_left)
+
+    def restart_game(self) -> None:
+        # setting up dynamic Settings
+        # reset Game stats
+        # update HUD scores
+        self._reset_level()
+        self.ship._center_ship()
+        self.game_active = True
+        pygame.mouse.set_visible(False)
 
     def _reset_level(self) -> None:
         self.ship.arsenal.arsenal.empty()
@@ -87,6 +99,11 @@ class AlienInvasion:
         self.screen.blit(self.bg, (0,0))
         self.ship.draw()
         self.alien_fleet.draw()
+
+        if not self.game_active:
+            self.play_button.draw()
+            pygame.mouse.set_visible(True)
+
         pygame.display.flip()
 
     def _check_events(self) -> None:
@@ -96,10 +113,17 @@ class AlienInvasion:
                 pygame.quit()
                 sys.exit()
             
-            elif event.type == pygame.KEYDOWN:
+            elif event.type == pygame.KEYDOWN and self.game_active == True:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                self._check_button_clicked()
+
+    def _check_button_clicked(self) -> None:
+        mouse_pos = pygame.mouse.get_pos()
+        if self.play_button.check_clicked(mouse_pos):
+            self.restart_game()
 
     def _check_keyup_events(self, event) -> None:
         if event.key == pygame.K_RIGHT:
