87cf2be797567602e50dce9831ff9a98d00b6fdc
Gaven <ghuynh@student.cscc.edu>
2026-04-26 17:01:22 -0400

    Import type checking into button.py and set up

--- button.py
+++ button.py
@@ -1,8 +1,12 @@
 import pygame.font
 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from alien_invasion import AlienInvasion
+
 class Button:
-    def __init__(self, game, msg) -> None:
+    def __init__(self, game: 'AlienInvasion', msg) -> None:
         self.game = game
         self.screen = game.screen
         self.boundaries = game.screen.get_rect()
-        
