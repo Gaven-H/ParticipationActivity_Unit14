430a97aa13c30c35c4c08a7c9fa20eb485f49da4
Gaven <ghuynh@student.cscc.edu>
2026-04-26 16:54:40 -0400

    Added font settings to settings

--- settings.py
+++ settings.py
@@ -32,3 +32,7 @@ class Settings:
         self.button_w = 200
         self.button_h = 50
         self.button_color = (0,135,50)
+
+        self.text_color = (255,255,255)
+        self.button_font_size = 48
+        self.HUD_font_size = 20
