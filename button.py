0249dbbf456a34b461a8f79a70c4463466139d7a
Gaven <ghuynh@student.cscc.edu>
2026-04-26 17:00:29 -0400

    Create references within Button class

--- button.py
+++ button.py
@@ -2,3 +2,7 @@ import pygame.font
 
 class Button:
     def __init__(self, game, msg) -> None:
+        self.game = game
+        self.screen = game.screen
+        self.boundaries = game.screen.get_rect()
+        
