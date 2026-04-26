dcdb58e5bb105c74b8eab52a30cc2e9d1bcdd9e2
Gaven <ghuynh@student.cscc.edu>
2026-04-26 18:05:45 -0400

    Fleshed out game stats to be updated by the game

--- game_stats.py
+++ game_stats.py
@@ -1,3 +1,37 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from alien_invasion import AlienInvasion
+
 class GameStats():
-    def __init__(self, ships_left) -> None:
-        self.ships_left = ships_left
+    def __init__(self, game: 'AlienInvasion') -> None:
+        self.game = game
+        self.settings = game.settings
+        self.max_score = 0
+        self.ships_left = self.settings.starting_ship_count
+        self.reset_stats()
+
+    def reset_stats(self) -> None:
+        self.score = 0
+        self.level = 1
+
+    def update(self, collisions) -> None:
+        # update score
+        self._update_score(collisions)
+        # update max_score
+        self._update_max_score()
+        # update hi_score
+
+    def _update_max_score(self) -> None:
+        if self.score > self.max_score:
+            self.max_score = self.score
+        # print(f'Max: {self.max_score}')    
+
+    def _update_score(self, collisions) -> None:
+        for alian in collisions.values():
+            self.score += self.settings.alien_points
+        # print(f'Basic: {self.score}')
+
+    def update_level(self) -> None:
+        self.level += 1
+        # print(self.level)
