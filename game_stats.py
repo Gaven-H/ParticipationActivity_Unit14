2f9fdcc3773e79ea658aadb85e15a4a83f2ebae8
Gaven <ghuynh@student.cscc.edu>
2026-04-26 23:00:22 -0400

    Fleshed out game stats to update the high score and save and read from a file

--- game_stats.py
+++ game_stats.py
@@ -1,3 +1,6 @@
+# from pathlib import Path
+import json
+
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -8,8 +11,30 @@ class GameStats():
         self.game = game
         self.settings = game.settings
         self.max_score = 0
+        self.init_saved_scores()
         self.ships_left = self.settings.starting_ship_count
         self.reset_stats()
+    
+    def init_saved_scores(self) -> None:
+        self.path = self.settings.scores_file
+        if self.path.exists() and self.path.stat.__sizeof__() > 80:
+            contents = self.path.read_text()
+            scores = json.loads(contents)
+            self.hi_score = scores.get('hi_score', 0)
+        else:
+            self.hi_score = 0
+            self.save_scores()
+            # save the file
+        
+    def save_scores(self) -> None:
+        scores = {
+            'hi_score': self.hi_score
+        }
+        contents = json.dumps(scores, indent=4)
+        try:
+            self.path.write_text(contents)
+        except FileNotFoundError as e:
+            print(f'File Not Found: {e}')
 
     def reset_stats(self) -> None:
         self.score = 0
@@ -21,11 +46,18 @@ class GameStats():
         # update max_score
         self._update_max_score()
         # update hi_score
+        self._update_hi_score()
 
     def _update_max_score(self) -> None:
         if self.score > self.max_score:
             self.max_score = self.score
-        # print(f'Max: {self.max_score}')    
+        # print(f'Max: {self.max_score}')
+
+    def _update_hi_score(self) -> None:
+        if self.score > self.hi_score:
+            self.hi_score = self.score
+            # print(f'High: {self.hi_score}')
+
 
     def _update_score(self, collisions) -> None:
         for alian in collisions.values():
